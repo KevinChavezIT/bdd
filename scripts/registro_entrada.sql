c94072925c591443f605c46f5c5ebcc5528983ae
KevinChavezIT <hc7657819@gmail.com>
2023-09-19 21:04:06 -0500

    tablas creadas

---
+++ scripts/registro_entrada.sql
@@ -0,0 +1,9 @@
+--tabla registro entrada
+
+create table registro_entrada (
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not null,
+	constraint registro_entrada_pk primary Key(codigo_registro)
+) 
